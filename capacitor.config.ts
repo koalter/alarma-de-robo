b47fe4005a6ffe7fd3c3beb76f5dec53688a2f07
Lorenzo Cea <lcea@hexacta.com>
2022-09-21 00:58:27 -0300

    diseño inicial

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'app.alarmaRobo.lcea',
   appName: 'alarma-de-robo',
   webDir: 'www',
   bundledWebRuntime: false
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'lcea-pps-2022',
+    appId: '1:193071352233:web:a366bde1d81a01acce9cb8',
+    storageBucket: 'lcea-pps-2022.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAUgl5_vKLpzOpEm0_Yx2lh4ftLlHXBMNU',
+    authDomain: 'lcea-pps-2022.firebaseapp.com',
+    messagingSenderId: '193071352233',
+  },
   production: false
 };
 
