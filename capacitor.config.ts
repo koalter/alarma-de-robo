7aff1ca5e352eb34264ef8cd31c6129ac561b219
Lorenzo Cea <lcea@hexacta.com>
2022-09-20 23:20:16 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'alarma-de-robo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
