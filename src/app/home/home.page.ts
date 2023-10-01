5af9d9370b56352461031dbb0cc4ba6f4d055a08
koalter <heavens_drive090@hotmail.com>
2023-10-01 16:09:36 -0300

    primera pantalla

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoadingController } from '@ionic/angular';
+import { AlertController, AlertInput, LoadingController, ModalController, ToastController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -9,22 +9,82 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  isActive: boolean = false;
+
   constructor(
     private auth: AuthService,
     private router: Router,
-    private loadingController: LoadingController
+    private loadingController: LoadingController,
+    private toastController: ToastController,
+    private alertController: AlertController
   ) {}
 
   async logout() {
-    const loadingElement = await this.loadingController
-      .create({ message: 'Cerrando sesión...' });
-      
-    await loadingElement.present();
-    const result = await this.auth.logout();
-    await loadingElement.dismiss();
+    if (await this.promptCredentials()) {
+      const loadingElement = await this.loadingController
+        .create({ message: 'Cerrando sesión...' });
+        
+      await loadingElement.present();
+      const result = await this.auth.logout();
+      await loadingElement.dismiss();
+  
+      if (result) {
+        this.isActive = false;
+        this.router.navigate(['login']);
+      }
+    }
+  }
 
-    if (result) {
-      this.router.navigate(['login']);
+  async promptCredentials() {
+    if (!this.isActive) {
+      return true;
+    }
+
+    const alert = await this.alertController.create({
+      header: 'Ingrese contraseña',
+      inputs: [
+        {
+          name: 'password',
+          placeholder: 'Contraseña',
+          type: 'password'
+        }
+      ],
+      buttons: [
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: async (data) => {
+            if (await this.auth.login(this.auth.user!.email!, data.password)) {
+              return true;
+            } else {
+              this.toastController.create({
+                message: 'Contraseña incorrecta',
+                duration: 2000,
+                position: 'top',
+                color: 'danger'
+              }).then(toast => toast.present());
+              return false;
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    return await alert.onDidDismiss();
+  }
+
+  async toggleAlarm() {
+    if (await this.promptCredentials()) {
+      this.isActive = !this.isActive;
+  
+      this.toastController.create({
+        message: this.isActive ? 'La alarma fue activada' : 'La alarma fue desactivada',
+        duration: 3000,
+        position: 'top',
+        color: this.isActive ? 'success' : 'danger'
+      }).then(toast => toast.present());
     }
   }
 }
