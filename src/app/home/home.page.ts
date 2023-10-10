0d71be7a8cb6c7a832eddc9ee071599d66507a01
koalter <heavens_drive090@hotmail.com>
2023-10-10 03:36:56 -0300

    pending sounds

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController, AlertInput, LoadingController, ModalController, ToastController } from '@ionic/angular';
+import { AlertController, LoadingController, ToastController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
+import { CapacitorFlash } from '@capgo/capacitor-flash';
+import { Haptics } from '@capacitor/haptics';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   isActive: boolean = false;
+  trigger: boolean = false;
+  x: any;
+  y: any;
+  z: any;
+
+  alarmType: 'left' | 'right' | 'vertical' | 'horizontal' | undefined;
 
   constructor(
     private auth: AuthService,
@@ -19,6 +27,29 @@ export class HomePage {
     private toastController: ToastController,
     private alertController: AlertController
   ) {}
+
+  ngOnInit() {
+    const accelerometer = (navigator as any).accelerometer;
+    accelerometer.watchAcceleration(
+      (acceleration: any) => {
+        this.x = (acceleration.x as number).toFixed(0);
+        this.y = (acceleration.y as number).toFixed(0);
+        this.z = (acceleration.z as number).toFixed(0);
+
+        if (this.isActive && this.z == 10 && (this.x == 0 || this.y == 0)) {
+          this.trigger = true;
+        }
+
+        this.handleAlarm();
+      },
+      () => {
+        console.log("error on accelerometer");
+      },
+      {
+        frequency: 500
+      }
+    );
+  }
 
   async logout() {
     if (await this.promptCredentials()) {
@@ -57,6 +88,7 @@ export class HomePage {
           role: 'confirm',
           handler: async (data) => {
             if (await this.auth.login(this.auth.user!.email!, data.password)) {
+              this.resetAlarm();
               return true;
             } else {
               this.toastController.create({
@@ -65,6 +97,11 @@ export class HomePage {
                 position: 'top',
                 color: 'danger'
               }).then(toast => toast.present());
+              Haptics.vibrate({ duration: 5000 });
+              CapacitorFlash.switchOn({ intensity: 1 })
+                .then(() => {
+                  setTimeout(() => CapacitorFlash.switchOff(), 5000);
+                });
               return false;
             }
           }
@@ -87,5 +124,38 @@ export class HomePage {
         color: this.isActive ? 'success' : 'danger'
       }).then(toast => toast.present());
     }
+  }
+
+  handleAlarm() {
+    if (this.trigger) {
+      if (this.z != 10) {
+        if (this.x < -2 && this.alarmType !== 'right') {
+          this.alarmType = 'right';
+        } else if (this.x > 2 && this.alarmType !== 'left') {
+          this.alarmType = 'left';
+        } else if ((this.y < -2 || this.y > 2) && this.alarmType !== 'vertical') {
+          this.alarmType = 'vertical';
+          CapacitorFlash.switchOn({ intensity: 1 })
+            .then(() => {
+              setTimeout(() => CapacitorFlash.switchOff(), 5000);
+            });
+        } 
+      } else {
+        if (this.alarmType == 'left' || this.alarmType == 'right' || this.alarmType == 'vertical') {
+          this.alarmType = 'horizontal';
+          Haptics.vibrate({ duration: 5000 });
+        }
+      }
+    }
+  }
+
+  resetAlarm() {
+    this.trigger = false;
+    this.alarmType = undefined;
+    CapacitorFlash.isSwitchedOn().then(result => {
+      if (result.value) {
+        CapacitorFlash.switchOff();
+      }
+    })
   }
 }
